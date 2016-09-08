c2781c67a2aba09fe922076af6ff36fb3c0fac93
ryandimick <ryan.dimick@gmail.com>
2016-09-08 17:35:47 -0500

    fixed entityframework reference issue
    added documentation to exceptions

--- SmartPong.Domain/Exceptions/InvalidMatchException.cs
+++ SmartPong.Domain/Exceptions/InvalidMatchException.cs
@@ -2,19 +2,44 @@
 
 namespace SmartPong.Exceptions
 {
+    /// <summary>
+    /// 
+    /// Represents an error that occurred during match processing.
+    /// 
+    /// </summary>
     public sealed class InvalidMatchException : SmartPongException
     {
+        /// <summary>
+        /// 
+        /// Initializes a new instance of the <see cref="InvalidMatchException"/> class.
+        /// 
+        /// </summary>
         public InvalidMatchException()
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// Initializes a new instance of the <see cref="InvalidMatchException"/> class with a specified error message.
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="message">The message that describes the error.</param>
         public InvalidMatchException(string message)
             : base(message)
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// Initiailizes a new instance of the <see cref="InvalidMatchException"/> class with a specified error message and a reference to the innter exception that is the cause of this exception.
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no exception is specified.</param>
         public InvalidMatchException(string message, Exception innerException)
             : base(message, innerException)
         {
--- SmartPong.Domain/Exceptions/SmartPongException.cs
+++ SmartPong.Domain/Exceptions/SmartPongException.cs
@@ -2,19 +2,44 @@
 
 namespace SmartPong.Exceptions
 {
+    /// <summary>
+    /// 
+    /// Represents errors that occur within SmartPong applications.
+    /// 
+    /// </summary>
     public class SmartPongException : Exception
     {
+        /// <summary>
+        /// 
+        /// Initializes a new instance of the <see cref="SmartPongException"/> class.
+        /// 
+        /// </summary>
         public SmartPongException()
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// Initializes a new instance of the <see cref="SmartPongException"/> class with a specified error message.
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="message">The message that describes the error.</param>
         public SmartPongException(string message) 
             : base(message)
         {
             
         }
 
+        /// <summary>
+        /// 
+        /// Initiailizes a new instance of the <see cref="SmartPongException"/> class with a specified error message and a reference to the innter exception that is the cause of this exception.
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no exception is specified.</param>
         public SmartPongException(string message, Exception innerException)
             : base(message, innerException)
         {
